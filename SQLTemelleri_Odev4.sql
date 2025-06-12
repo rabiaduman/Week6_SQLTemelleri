6db7afc504a6cd47a0156c13b44bbcf8f1ea337b
rabiaduman <rbiaduman@gmail.com>
2025-06-12 15:48:19 +0300

    Add files via upload

---
+++ SQLTemelleri_Odev4.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT replacement_cost FROM film ; --film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sırala
+SELECT COUNT (DISTINCT replacement_cost) FROM film ; --film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri var
+SELECT COUNT(*) FROM film
+WHERE title LIKE 'T%' AND rating = 'G' ; --film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+SELECT COUNT(*) FROM country
+WHERE country LIKE '_____' ; --country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+SELECT COUNT(*) FROM city
+WHERE city ILIKE '%R'; --city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
