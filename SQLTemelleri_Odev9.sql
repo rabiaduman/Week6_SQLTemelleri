1896d23de6ad2c1b9a4298474ab9f95baf74e224
rabiaduman <rbiaduman@gmail.com>
2025-06-17 14:53:37 +0300

    Add files via upload

---
+++ SQLTemelleri_Odev9.sql
@@ -0,0 +1,12 @@
+
+--city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusu
+SELECT * FROM city
+INNER JOIN country ON city.country_id = country.country_id ;
+
+--customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusu
+SELECT payment_id , first_name , last_name FROM customer
+INNER JOIN payment ON customer.customer_id = payment.customer_id ;
+
+--customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusu
+SELECT rental_id , first_name , last_name FROM customer
+INNER JOIN rental ON customer.customer_id = rental.customer_id ;
